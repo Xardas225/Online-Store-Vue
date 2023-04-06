a4c19a58ffa8d3f3bc61a2273d0b77d18d047458
Alexander Derevkov <78501617+Xardas225@users.noreply.github.com>
2023-04-06 17:39:23 +0300

    change layout profile

--- src/pages/v-profile.vue
+++ src/pages/v-profile.vue
@@ -1,31 +1,82 @@
 <template>
   <v-container>
-    <vInput :input_data="this.name"></vInput>
+    <h3 class="profile__title text-h4">Профиль</h3>
+    <v-row>
+      <v-col>
+        <v-card class="profile__card" max-width="400" min-height="300">
+          <v-container>
+            <v-row>
+              <v-col cols="6">
+                <img
+                  class="profile__img"
+                  :src="require(`../assets/images/squirrel.jpg`)"
+                />
+              </v-col>
+              <v-col cols="6" class="profile__bio">
+                {{ name }}
+              </v-col>
+            </v-row>
+            <v-row class="profile__desc">
+              <v-col cols="12"> Email: {{ email }} </v-col>
+              <v-col cols="12"> Phone: {{ phone }} </v-col>
+              <v-col cols="12"> City: {{ city }} </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="profile__card" min-height="300">
+          <v-container>
+            <v-card-title class="text-h5">История покупок</v-card-title>
+            <v-row v-if="!buy.length">
+              Покупок нет
+            </v-row>
+            <v-row v-else>
+
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import vInput from "@/components/FormInputs/v-base-input.vue";
-
 export default {
   name: "v-profile",
-  components: {
-    vInput,
-  },
+  components: {},
   data() {
     return {
-      name: {
-        label: "Введите имя",
-      },
-      password: {
-        label: "Введите пароль",
-      },
-      email: {
-        label: "Введите пароль",
-      },
+      name: "Александр",
+      email: "alexanderderevkov@yandex.ru",
+      phone: "89223149134",
+      city: "Волжский",
+      buy: []
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile__title {
+  text-align: left;
+}
+.profile__card {
+  margin-top: 20px;
+}
+.profile__img {
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+}
+.profile__bio {
+  text-align: left;
+  font-weight: 600;
+  font-size: 20px;
+}
+.profile__desc {
+  text-align: left;
+  color: rgb(86, 86, 88);
+  font-size: 16px;
+}
+</style>
